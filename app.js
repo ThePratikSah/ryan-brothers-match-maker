6beaf5a2ec0f6edb78bb654ab95239f0e8c5f66e
Shashank Biplav <Biplavshashank7@gmail.com>
2020-09-06 22:41:34 +0530

    Installed required packages and configured app.js

--- app.js
+++ app.js
@@ -1,3 +1,81 @@
 const express = require('express');
 
-const router = express.Router();
+const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
+
+const multer = require('multer');
+
+const helmet = require('helmet');
+
+const compression = require('compression');
+
+require('dotenv').config();
+
+const port = process.env.PORT|| 5500;
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + '-' + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpeg') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+app.use(bodyParser.json());
+
+app.use(multer({
+    storage: fileStorage,
+    fileFilter: fileFilter
+}).single('image'));
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.use(helmet());
+
+app.use(compression());
+
+//central error handling middleware
+app.use((error, req, res, next) => {
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({
+        message: message,
+        data: data
+    });
+});
+
+mongoose.connect(process.env.MONGODB_URI,{
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+})
+    .then(()=>{
+        // console.log('Connection to DB');
+        app.listen(port,
+            //  ()=>{
+            //     console.log(`listening on port${port}`);
+            // }
+        );
+    }).catch(err=>{
+    console.log(err);
+});
