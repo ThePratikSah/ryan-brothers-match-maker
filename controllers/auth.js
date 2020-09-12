96f2a547a8b6f521aa6e4e9e6b1a1be44dd8f145
Shashank Biplav <Biplavshashank7@gmail.com>
2020-09-12 07:57:05 +0530

    Added functionality to reset password

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,17 @@ const User = require('../models/user');
 
 const Administrator = require('../models/administrator');
 
+//Initializing SIB API
+const SibApiV3Sdk = require('sib-api-v3-sdk');
+let defaultClient = SibApiV3Sdk.ApiClient.instance;
+// Configure API key authorization: api-key
+let apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = process.env.SIB_AUTH_API_KEY;
+// Configure API key authorization: partner-key
+let partnerKey = defaultClient.authentications['partner-key'];
+partnerKey.apiKey = process.env.SIB_PARTNER_API_KEY;
+let apiInstance = new SibApiV3Sdk.SMTPApi();
+
 exports.userSignup = async (req, res, next) => {
     const errors = expressValidator.validationResult(req);
     if (!errors.isEmpty()) {
@@ -152,4 +163,164 @@ exports.administratorLogin = async (req, res, next) => {
         }
         next(err);
     }
+};
+
+exports.getOTPforAdmin = async (req, res, next) => {
+    const email = req.body.email;
+    let loadedAdmin;
+    let generatedOTP;
+    try {
+        const admin = await Administrator.findOne({email: email});
+        if (!admin) {
+            const error = new Error('Admin with this email doesn\'t exist');
+            error.statusCode = 401;
+            throw error;
+        }
+        loadedAdmin = admin;
+        generatedOTP = generateOTP();
+        admin.resetToken = generatedOTP;
+        admin.resetTokenExpiryDate = Date.now() + 3600000;
+        await admin.save();
+        const data = await sendEmailToResetPassword(email, loadedAdmin.name, generatedOTP);
+        res.status(200).json({
+            message: 'OTP sent!',
+            result: data
+        });
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+};
+
+exports.getOTPforUser = async (req, res, next) => {
+    const email = req.body.email;
+    let loadedUser;
+    let generatedOTP;
+    try {
+        const user = await User.findOne({email: email});
+        if (!user) {
+            const error = new Error('User with this email doesn\'t exist');
+            error.statusCode = 401;
+            throw error;
+        }
+        loadedUser = user;
+        generatedOTP = generateOTP();
+        user.resetToken = generatedOTP;
+        user.resetTokenExpiryDate = Date.now() + 3600000;
+        await user.save();
+        const data = await sendEmailToResetPassword(email, loadedUser.name, generatedOTP);
+        res.status(200).json({
+            message: 'OTP sent!',
+            result: data
+        });
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+};
+
+exports.resetAdminPassword = async (req, res, next) => {
+    const email = req.body.email;
+    const oneTimePassword = req.body.otp;
+    const password = req.body.password;
+    try {
+        const admin = await Administrator.findOne({
+            resetToken: oneTimePassword,
+            resetTokenExpiryDate: {
+                $gt: Date.now()
+            },
+            email: email
+        })
+        if (!admin) {
+            const error = new Error('Admin with this email doesn\'t exist');
+            error.statusCode = 401;
+            throw error;
+        }
+        const hashedPassword = await bcrypt.hash(password, 12);
+        admin.password = hashedPassword;
+        admin.resetToken = undefined;
+        admin.resetTokenExpiryDate = undefined;
+        const data =await admin.save();
+        res.status(201).json({
+            message: 'Password Updated!',
+            result: data
+        });
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+};
+
+exports.resetUserPassword = async (req, res, next) => {
+    const email = req.body.email;
+    const oneTimePassword = req.body.otp;
+    const password = req.body.password;
+    try {
+        const user = await User.findOne({
+            resetToken: oneTimePassword,
+            resetTokenExpiryDate: {
+                $gt: Date.now()
+            },
+            email: email
+        })
+        if (!user) {
+            const error = new Error('User with this email doesn\'t exist');
+            error.statusCode = 401;
+            throw error;
+        }
+        console.log("admin.resetTokenExpiryDate"+user.resetTokenExpiryDate);
+        console.log("Date.now()" + Date.now());
+        const hashedPassword = await bcrypt.hash(password, 12);
+        user.password = hashedPassword;
+        user.resetToken = undefined;
+        user.resetTokenExpiryDate = undefined;
+        const data =await user.save();
+        res.status(201).json({
+            message: 'Password Updated!',
+            result: data
+        });
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+};
+
+//TODO: ADD Reset Password functionality
+//helper function to generate AlphaNumeric OTP 8 characters
+const generateOTP = () => {
+    const string = '0123456789';
+    let OTP = '';
+    const len = string.length;
+    for (let i = 0; i < 5; i++) {
+        OTP += string[Math.floor(Math.random() * len)];
+    }
+    return OTP;
+};
+
+//helper function to send OTP via SIB
+const sendEmailToResetPassword = async (email, name, resetOTP) => {
+    const sendSmtpEmail = {
+        to: [{
+            email: email,
+            name: name
+        }],
+        templateId: 5,
+        params: {
+            EMAIL: email,
+            RESETLINK: `${resetOTP}`
+        },
+        subject: 'Reset Password OTP'
+    };
+
+    const data = await apiInstance.sendTransacEmail(sendSmtpEmail);
+    console.log('API call successful. Email Sent. Returned data: ' + data);
+    return data;
 };
